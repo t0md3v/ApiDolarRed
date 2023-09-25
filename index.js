122db731280c1a767887f97dbc75a2cdeb6e0067
Sebastian Gallego <sebastian.gallego@gmail.com>
2023-09-24 23:09:16 -0300

    getDolar

---
+++ index.js
@@ -0,0 +1,19 @@
+const dOficial = "https://dolarapi.com/v1/dolares/oficial";
+const dBlue = "https://dolarapi.com/v1/dolares/blue";
+const dTarjeta = "https://dolarapi.com/v1/dolares/solidario";
+
+const options = { method: "GET", headers: { Accept: "application/json" } };
+
+async function getDolar(url) {
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getDolar(dOficial);
+getDolar(dBlue);
+getDolar(dTarjeta);

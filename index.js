9b100db9500e7d03c7943d66de56736b2ec75929
t0md3v <tom.infotek@gmail.com>
2023-11-26 01:33:41 -0300

    test calc

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function crearTarjetas(cotizacion) {
 
 fetch("https://dolarapi.com/v1/dolares/bolsa")
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => console.log(data))
   displayMep(data)
 
 function displayMep(data) {

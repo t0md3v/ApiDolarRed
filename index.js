20acf2399df16c00ad63f514c8a10e13be2ad8a5
t0md3v <tom.infotek@gmail.com>
2023-11-26 00:00:36 -0300

    add variable dRed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dBlue = "https://dolarapi.com/v1/dolares/blue";
 const dTarjeta = "https://dolarapi.com/v1/dolares/solidario";
 const dMep = "https://dolarapi.com/v1/dolares/bolsa";
 const dRed = dMep + dBlue
+document.getElementById("dRed").innerHTML = dRed
 const options = { method: "GET", headers: { Accept: "application/json" } };
 
 const actualizacion = document.getElementById("fechaHora");

89799adddd1bc127b964f3619c2c511fe01f07da
t0md3v <tom.infotek@gmail.com>
2023-11-26 01:47:12 -0300

    test calc

--- testmep.js
+++ testmep.js
@@ -1,7 +1,7 @@
 fetch("https://dolarapi.com/v1/dolares/bolsa")
   .then(response => response.json())
-  .then(data => document.getElementById("dolarMep"))
-  //displayMep(data)
+  .then(data => console.log(data))
+  displayMep(data)
 
 function displayMep(data) {
   const dolarMep = data.compra;

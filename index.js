c0b02b46757f978731d9133609111e86a0594f16
t0md3v <tom.infotek@gmail.com>
2023-11-26 01:06:28 -0300

    add dMep to html

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function crearTarjetas(cotizacion) {
   // Agrego la tarjeta al contenedor
   cotizacionesContainer.appendChild(tarjeta);
 }
-document.getElementById("dRed").innerHTML = dMep.cotizacion.venta
+document.getElementById("dRed").innerHTML = data.venta
 
 /*
 Alternative function for MEP dolar

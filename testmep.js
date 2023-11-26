a98147832f5b3c0defb1ce243c8b31c9dbf63ae1
t0md3v <tom.infotek@gmail.com>
2023-11-26 14:09:49 -0300

    pull

--- testmep.js
+++ testmep.js
@@ -2,10 +2,13 @@ function dolarRedFunc() {
     let getMep = document.getElementById("dolarMep")
     let getCad = document.getElementById("usd-cad")
     let red = document.getElementById("dolarRed")
+    let cad
+    let mep
     fetch("https://dolarapi.com/v1/dolares/bolsa")
         .then(response => response.json())
         .then(data => {
         getMep.textContent = Number(data.compra);
+
     });
 
     fetch("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/cad.json")
@@ -15,8 +18,7 @@ function dolarRedFunc() {
   });
   //let redTest = getMep.textContent/getCad.textContent
   //console.log(redTest)  
-  let cad = getCad.value
-    let mep = getMep.value
+
     document.getElementById("dolarRed").innerHTML = mep/cad
     console.log(mep/cad)
 }

c9f7add5fc72768cb8db1f294c7abde5234f1314
t0md3v <tom.infotek@gmail.com>
2023-11-26 03:00:51 -0300

    testmep function

--- testmep.js
+++ testmep.js
@@ -1,20 +1,21 @@
-let getMep = document.getElementById("dolarMep")
-let getCad = document.getElementById("usd-cad")
-let red // = getMep/getCad
-red.textContent = getMep/getCad
-red = document.getElementById("dolarRed")
+function dolarRedFunc() {
+    let getMep = document.getElementById("dolarMep")
+    let getCad = document.getElementById("usd-cad")
+    let red = document.getElementById("dolarRed")
+    fetch("https://dolarapi.com/v1/dolares/bolsa")
+        .then(response => response.json())
+        .then(data => {
+        getMep.textContent = Number(data.compra);
+    })
 
-fetch("https://dolarapi.com/v1/dolares/bolsa")
-  .then(response => response.json())
-  .then(data => {
-    getMep.textContent = Number(data.compra);
+    fetch("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/cad.json")
+        .then(response => response.json())
+        .then(data => {
+        getCad.textContent = Number(data.cad)
   })
-
-fetch("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/cad.json")
-  .then(response => response.json())
-  .then(data => {
-    getCad.textContent = Number(data.cad)
-  })
+  red = getMep/getCad
+}
+dolarRedFunc()
 
 /*
 function displayMep(data) {

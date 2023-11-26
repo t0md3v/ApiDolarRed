9615659ea08857cddfa512384665fcef7a0e9f4c
t0md3v <tom.infotek@gmail.com>
2023-11-26 01:11:27 -0300

    test calc

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function crearTarjetas(cotizacion) {
   // Agrego la tarjeta al contenedor
   cotizacionesContainer.appendChild(tarjeta);
 }
-document.getElementById("dRed").innerHTML = getDolar(dMep).data
+document.getElementById("dRed").innerHTML = data.getDolar(dMep)
 
 /*
 Alternative function for MEP dolar
